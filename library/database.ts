eed7ab527c6d0c0bf1cc225cd3dcbef8abf84cae
juliusyam <juliusyam@users.noreply.github.com>
2021-12-22 16:03:05 +0000

    node 16.13.1, generate form entry id and css tweaks

--- library/database.ts
+++ library/database.ts
@@ -1,12 +1,16 @@
-import { firebaseApp } from "../library/firebase";
-import { getFirestore, addDoc, collection } from "firebase/firestore";
+import {firebaseApp} from "../library/firebase";
+import { doc, getFirestore, setDoc } from "firebase/firestore";
+import jessicaDay from 'dayjs';
+import {ContactFormValues} from "../pages/contact";
 
 const firestore = getFirestore(firebaseApp);
 
-import {ContactFormValues} from "../pages/contact";
+function generateId() {
+  return jessicaDay().format('DD-MM-YY_HH-mm-ss')
+}
 
 export async function sendForm(data: ContactFormValues, onSuccess?: () => void, onError?: () => void) {
-  return await addDoc(collection(firestore, "contact"), data)
+  return await setDoc(doc(firestore, "contact", generateId()), data)
     .then(onSuccess)
     .catch(onError);
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,9 @@
 import React from 'react';
 import {Layout} from "../components/Layout";
 import {Color} from "../utilities/color";
-import ReelVisionLogo from "../public/images/ReelVisionLogo";
 import {Divider} from "../components/smallcomponents/Divider";
 import {AboutUs, SiteInfo} from "../data/dataStructure";
-import { AboutUsContainer } from "../components/AboutUsContainer";
+import {AboutUsContainer} from "../components/AboutUsContainer";
 import {useRouter} from "next/router";
 
 // Retrieve data
@@ -28,9 +27,6 @@ function InitialContainer() {
     <div className="initial-container">
       <div className="home-background-image" />
       <section className="home-main-section">
-        <div className="svg-container">
-          <ReelVisionLogo />
-        </div>
         <h1>{ siteInfo.title }</h1>
         <Divider background={ Color.Primary } maxWidth="10em" width="100%" height="0.3em" />
         <h3>{ siteInfo.subtitle }</h3>
@@ -48,6 +44,6 @@ function ShowReelButton() {
   const router = useRouter();
 
   return (
-    <button className="showreel-button" onClick={ () => router.push('/showreel') }><p>Showreel</p></button>
+    <button className="showreel-button" onClick={ () => router.push('/showreel') }><p>View Showreel</p></button>
   )
 }
